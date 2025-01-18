15aeff90bf92820d9909348d0caded0c51079318
xforcevesa <nomodeset@qq.com>
2025-01-18 17:27:28 +0800

    fix cfg

--- classification/train-vrwkv7.sh
+++ classification/train-vrwkv7.sh
@@ -1,5 +1,5 @@
 python -m torch.distributed.launch \
     --nnodes=1 --node_rank=0 --nproc_per_node=8 \
     --master_addr="127.0.0.1" --master_port=29509 \
-    main.py --batch-size 8 --data-path ./imagenet/ \
+    main.py --batch-size 12 --data-path ./imagenet/ \
     --output ./tmp/ --cfg configs/vrwkv7/vrwkv7v_tiny_224.yaml
